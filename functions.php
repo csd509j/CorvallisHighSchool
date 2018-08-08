e70d491d3ee96183efff21ee1592b7770870f065
Josh Armentano <josh@abidewebdesign.com>
2018-08-08 15:11:46 -0700

    Bug fixes

--- functions.php
+++ functions.php
@@ -92,3 +92,122 @@ function languages_toggle(){
 	
 <?php
 }
+/**
+ * Add option menus
+ *
+ * @since CSD Schools 1.0
+ */
+ 
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page();
+	
+}
+if( function_exists('acf_add_options_sub_page') ) {
+    acf_add_options_sub_page( 'General' );
+    acf_add_options_sub_page( 'Pages' );
+    acf_add_options_sub_page( 'Calendar' );
+    acf_add_options_sub_page( 'Footer' );
+    acf_add_options_sub_page( '404 Page' );
+    acf_add_options_sub_page( 'District Info' );
+}
+
+/**
+ * Load sidebar select fields with callout blocks from options
+ *
+ * @since CSD Schools 1.0
+ */
+ 
+ function acf_load_sidebar_callout_blocks_field_choices( $field ) {
+    
+    // reset choices
+    $field['choices'] = array();
+
+    // if has rows
+    if( have_rows('callout_blocks', 'option') ) {
+        
+        // while has rows
+        while( have_rows('callout_blocks', 'option') ) {
+            
+            // instantiate row
+            the_row();
+            
+            // vars
+            $value = get_sub_field('callout_block_heading');
+            $label = get_sub_field('callout_block_heading');
+
+            
+            // append to choices
+            $field['choices'][ $value ] = $label;
+            
+        }
+        
+    }
+
+    // return the field
+    return $field;
+    
+} 
+add_filter('acf/load_field/name=sidebar_callout_blocks', 'acf_load_sidebar_callout_blocks_field_choices');
+
+/**
+ * Load sidebar select fields with contact blocks from options
+ *
+ * @since CSD Schools 1.0
+ */
+ 
+function acf_load_sidebar_contact_blocks_field_choices( $field ) {
+    
+    // reset choices
+    $field['choices'] = array();
+
+    // if has rows
+    if( have_rows('contact_blocks', 'option') ) {
+        
+        // while has rows
+        while( have_rows('contact_blocks', 'option') ) {
+            
+            // instantiate row
+            the_row();
+            
+            // vars
+            $value = get_sub_field('contact_name');
+            $label = get_sub_field('contact_name');
+
+            
+            // append to choices
+            $field['choices'][ $value ] = $label;
+            
+        }
+        
+    }
+
+    // return the field
+    return $field;
+    
+} 
+add_filter('acf/load_field/name=sidebar_contact_block', 'acf_load_sidebar_contact_blocks_field_choices');
+
+/**
+ * Set featured image from ACF field
+ *
+ * @since CSD Schools 1.0
+ */
+ 
+function acf_set_featured_image( $value, $post_id, $field  ){
+	
+	$id = $value;
+	
+	if( ! is_numeric( $id ) ){
+		
+		$data = json_decode( stripcslashes($id), true );
+		$id = $data['cropped_image'];
+	
+	}
+	
+	update_post_meta( $post_id, '_thumbnail_id', $id );
+	
+	return $value;
+}
+// acf/update_value/name={$field_name} - filter for a specific field based on it's name
+add_filter( 'acf/update_value/name=featured_image', 'acf_set_featured_image', 10, 3 );
